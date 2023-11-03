96c16dc68c43019166d4dc56ba1bf03c3603ecd1
Yue-cn <2777174292@qq.com>
2023-11-03 19:59:01 +0800

    添加项目文件。

---
+++ src/App.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace OpenFrp.Launcher
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var wind = new MainWindow();
+
+            wind.Show();
+
+            if (Awe.UI.Win32.UserUxtheme.IsSupportDarkMode)
+            {
+                Awe.UI.Win32.UserUxtheme.AllowDarkModeForApp(true);
+                Awe.UI.Win32.UserUxtheme.ShouldAppsUseDarkMode();
+                Awe.UI.Win32.UserUxtheme.ShouldSystemUseDarkMode();
+            }
+
+            var handle = new WindowInteropHelper(wind).EnsureHandle();
+
+            if (Environment.OSVersion.Version.Major >= 10)
+            {
+                int backdropPvAttribute = (int)Awe.UI.Win32.DwmApi.DWMSBT.DWMSBT_TABBEDWINDOW;
+                Awe.UI.Win32.DwmApi.DwmSetWindowAttribute(handle, Awe.UI.Win32.DwmApi.DWMWINDOWATTRIBUTE.DWMWA_SYSTEMBACKDROP_TYPE,
+                    ref backdropPvAttribute,
+                    Marshal.SizeOf(typeof(int)));
+
+                var pvAttribute = (int)Awe.UI.Win32.DwmApi.PvAttribute.Enable;
+                var dwAttribute = Awe.UI.Win32.DwmApi.DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE;
+
+                if (Environment.OSVersion.Version < new Version(10, 0, 18985))
+                {
+                    dwAttribute = Awe.UI.Win32.DwmApi.DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE_OLD;
+                }
+
+                var vk = Awe.UI.Win32.DwmApi.DwmSetWindowAttribute(handle, dwAttribute,
+                    ref pvAttribute,
+                    Marshal.SizeOf(typeof(int)));
+
+
+                if (vk is not 0)
+                {
+                    wind.Tag = "non-success-dark";
+                }
+                else
+                {
+                    wind.Width += 1;
+                    wind.Width -= 1;
+                }
+
+            }
+
+            
+
+            Awe.UI.Win32.UserUxtheme.SetWindowLong(handle, -16, Awe.UI.Win32.UserUxtheme.GetWindowLong(handle, -16) & ~0x80000);
+
+              
+        }
+
+        
+    }
+}
---
+++ src/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace OpenFrp.Launcher.ViewModels
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+
+        private ContentControl? _frame;
+
+        private bool CanFrameLoadExecute() => _frame is null;
+
+        [RelayCommand(CanExecute = nameof(CanFrameLoadExecute))]
+        private void @event_FrameLoaded(RoutedEventArgs e)
+        {
+            if (e.Source is ContentControl fr)
+            {
+                _frame = fr;
+
+                event_FrameLoadedCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        [RelayCommand]
+        private async Task @event_SidebarItemInvoked(Type page)
+        {
+
+            VisualStateManager.GoToElementState(_frame, "_fr_Doing",false);
+
+            await Task.Delay(125);
+
+            if (_frame is not null && page is not null)
+                _frame.Content = Activator.CreateInstance(page);
+
+            VisualStateManager.GoToElementState(_frame, "_fr_Did", false);
+        }
+    }
+}
